a90f519749cdf9c83fd758dda040e3096c93e136
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-01 18:56:47 -0300

    feat: setup native base and fonts loading

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { NativeBaseProvider, Text, View } from "native-base";
+import * as SplashScreen from "expo-splash-screen";
 
+import { THEME } from "./src/styles/theme";
+import { SignIn } from "@screens/signin";
+
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "SF-Pro-Display-Bold": require("./assets/fonts/SF-Pro-Display-Bold.otf"),
+    "SF-Pro-Display-Semibold": require("./assets/fonts/SF-Pro-Display-Semibold.otf"),
+    "SF-Pro-Text-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+    "SF-Pro-Text-Medium": require("./assets/fonts/SF-Pro-Text-Medium.otf"),
+    "SF-Pro-Text-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <View flex={1} onLayout={onLayoutRootView}>
+        <SignIn />
+      </View>
+      <StatusBar style="light" translucent />
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
