960f9078377505067100bba7254d2ee5d7a5ec61
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-02 00:11:06 -0300

    feat: create button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,32 @@
+import { Button as NativeButton, IButtonProps } from "native-base";
+
+interface ButtonProps extends IButtonProps {}
+
+export function Button({ children, ...rest }: ButtonProps) {
+  return (
+    <NativeButton
+      w="full"
+      mt="6"
+      py="4"
+      size="lg"
+      bgColor="blue.400"
+      color="white"
+      borderRadius={8}
+      _text={{
+        textAlign: "center",
+        fontFamily: "semibold",
+        fontSize: "md",
+      }}
+      _pressed={{
+        bgColor: "blue.300",
+      }}
+      _disabled={{
+        bgColor: "gray.400",
+        color: "gray.200",
+      }}
+      {...rest}
+    >
+      {children}
+    </NativeButton>
+  );
+}
