54f77e25c9af093c38b5ca3c3dbddce76a41397c
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-02 00:11:39 -0300

    refactor: uses custom component for input and button

--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -1,81 +1,36 @@
-import { Box, Button, Center, Input, Link } from "native-base";
+import { Center, KeyboardAvoidingView, Link } from "native-base";
 
 import Logo from "@assets/logo/vertical_white.svg";
+import { TextInput } from "@components/input";
+import { Button } from "@components/button";
 
 export function SignIn() {
   return (
-    <Center
-      flex={1}
-      bgColor="gray.900"
-      alignItems="center"
-      justifyContent="center"
-      paddingX="4"
-    >
-      <Logo />
-      <Input
-        mt="16"
-        px="5"
-        py="4"
-        bgColor="gray.600"
-        borderWidth={0}
-        borderRadius={8}
-        placeholder="Nome de usuário"
-        placeholderTextColor="gray.300"
-        color="gray.100"
-        fontFamily="body"
-        fontSize="lg"
-        _focus={{
-          borderWidth: 2,
-          borderColor: "blue.400",
-        }}
-      />
-      <Input
-        mt="4"
-        px="5"
-        py="4"
-        bgColor="gray.600"
-        borderWidth={0}
-        borderRadius={8}
-        placeholder="Senha"
-        placeholderTextColor="gray.300"
-        color="gray.100"
-        fontFamily="body"
-        fontSize="lg"
-        _focus={{
-          borderWidth: 2,
-          borderColor: "blue.400",
-        }}
-      />
-      <Link
-        mt="2"
-        ml="auto"
-        _text={{
-          fontFamily: "body",
-          fontSize: "sm",
-          color: "gray.300",
-          textAlign: "right",
-        }}
+    <KeyboardAvoidingView flex={1} w="full" behavior="padding">
+      <Center
+        flex={1}
+        bgColor="gray.900"
+        alignItems="center"
+        justifyContent="center"
+        paddingX="4"
       >
-        Esqueceu sua senha?
-      </Link>
-      <Button
-        mt="6"
-        py="4"
-        size="lg"
-        bgColor="blue.400"
-        borderRadius={8}
-        _text={{
-          textAlign: "center",
-          fontFamily: "semibold",
-          fontSize: "md",
-        }}
-        _pressed={{
-          bgColor: "blue.300",
-        }}
-        w="full"
-      >
-        Iniciar sessão
-      </Button>
-    </Center>
+        <Logo />
+        <TextInput mt="16" placeholder="Nome de usuário" />
+        <TextInput mt="4" placeholder="Senha" type="password" />
+        <Link
+          mt="2"
+          ml="auto"
+          _text={{
+            fontFamily: "body",
+            fontSize: "sm",
+            color: "gray.300",
+            textAlign: "right",
+          }}
+        >
+          Esqueceu sua senha?
+        </Link>
+        <Button isDisabled>Iniciar sessão</Button>
+      </Center>
+    </KeyboardAvoidingView>
   );
 }
