c155f72dcbe84e54304e3803f4c19bbeb3b0af1a
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-03 16:03:29 -0300

    feat: add default stack navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import { useCallback } from "react";
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { NativeBaseProvider, Text, View } from "native-base";
+import { NativeBaseProvider, View } from "native-base";
 import * as SplashScreen from "expo-splash-screen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { THEME } from "./src/styles/theme";
 import { SignIn } from "@screens/signin";
 
+import { THEME } from "./src/styles/theme";
+
 SplashScreen.preventAutoHideAsync();
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     "SF-Pro-Display-Bold": require("./assets/fonts/SF-Pro-Display-Bold.otf"),
@@ -30,7 +36,11 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <View flex={1} onLayout={onLayoutRootView}>
-        <SignIn />
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="login" component={SignIn} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </View>
       <StatusBar style="light" translucent />
     </NativeBaseProvider>
