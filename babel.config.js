5b23e16766681dae3492a28fd71d620ed03fed3a
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-03 16:43:58 -0300

    chore: add modules folder to babel config to fast import

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
-        'module-resolver',
+        "module-resolver",
         {
-          'root': ['./src'],
-          'alias': {
-            '@assets': './src/assets',
-            '@screens': './src/screens',
-            '@components': './src/components',
-            '@routes': './src/routes'
-          }
-        }
-      ]
-    ]
+          root: ["./src"],
+          alias: {
+            "@assets": "./src/assets",
+            "@screens": "./src/screens",
+            "@components": "./src/components",
+            "@routes": "./src/routes",
+            "@models": "./src/models",
+          },
+        },
+      ],
+    ],
   };
 };
