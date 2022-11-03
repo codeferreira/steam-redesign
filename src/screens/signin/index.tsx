4a97871643e6317cc93e5ff603f170dc03484c32
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-03 16:33:23 -0300

    feat: add form validation and navigation

--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -1,10 +1,41 @@
 import { Center, KeyboardAvoidingView, Link } from "native-base";
+import { Controller, useForm } from "react-hook-form";
+import { RouteProp, useNavigation } from "@react-navigation/native";
+import { useCallback } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-import Logo from "@assets/logo/vertical_white.svg";
 import { TextInput } from "@components/input";
 import { Button } from "@components/button";
 
-export function SignIn() {
+import Logo from "@assets/logo/vertical_white.svg";
+import { RouteProps } from "src/types/routes.type";
+
+const schema = yup
+  .object({
+    username: yup.string().required("Nome de usuário é obrigatório"),
+    password: yup.string().required("Senha é obrigatório"),
+  })
+  .required();
+
+type FormData = {
+  username: string;
+  password: string;
+};
+
+export function SignIn({ navigation }: RouteProps) {
+  const {
+    control,
+    handleSubmit,
+    formState: { dirtyFields },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const handleLogin = useCallback(({ username, password }: FormData) => {
+    navigation.navigate("Guard");
+  }, []);
+
   return (
     <KeyboardAvoidingView flex={1} w="full" behavior="padding">
       <Center
@@ -15,8 +46,37 @@ export function SignIn() {
         paddingX="4"
       >
         <Logo />
-        <TextInput mt="16" placeholder="Nome de usuário" />
-        <TextInput mt="4" placeholder="Senha" type="password" />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              mt="16"
+              placeholder="Nome de usuário"
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="username"
+        />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              mt="4"
+              placeholder="Senha"
+              type="password"
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="password"
+        />
         <Link
           mt="2"
           ml="auto"
@@ -29,7 +89,11 @@ export function SignIn() {
         >
           Esqueceu sua senha?
         </Link>
-        <Button mt="6" isDisabled>
+        <Button
+          mt="6"
+          isDisabled={!dirtyFields.username || !dirtyFields.password}
+          onPress={handleSubmit(handleLogin)}
+        >
           Iniciar sessão
         </Button>
       </Center>
