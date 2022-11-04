c3308eed18d7dd29f66f92421e4b83056ae5bebc
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-04 14:32:17 -0300

    chore: add bottom tab types

--- src/models/routes.type.ts
+++ src/models/routes.type.ts
@@ -1,8 +1,17 @@
+import { NavigatorScreenParams } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+export type BottomTabParamList = {
+  Guard: undefined;
+  Chat: undefined;
+  Store: undefined;
+  Community: undefined;
+  More: undefined;
+};
 
 export type RootStackParamList = {
   Login: undefined;
-  Guard: undefined;
+  App: NavigatorScreenParams<BottomTabParamList>;
 };
 
 export type RouteProps = NativeStackScreenProps<
